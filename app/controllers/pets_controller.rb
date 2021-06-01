8c63206c520e7808d82ccdb87b567d13badf62a9
Lrd134 <larryc3200@gmail.com>
2021-05-31 20:30:49 -0400

    commit tree d9b19066c1628a0be743325b5e885cb6d1bdd80f

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,21 +6,41 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    
+    
+    if !params[:pet][:owner_id]
+      @owner = Owner.create(name: params[:owner][:name])
+      @pet = Pet.create(name: params[:pet][:name], owner: @owner)
+    else
+      @pet = Pet.create(name: params[:pet][:name], owner: Owner.find(params[:pet][:owner_id]))
+    end
     redirect to "pets/#{@pet.id}"
   end
-
+  get '/pets/:id/edit' do 
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
   patch '/pets/:id' do 
-
+    @pet = Pet.find(params[:id])
+    if params[:owner][:name] == ""
+      @pet.update(params[:pet])
+    else
+      
+      params[:pet][:owner_id] = Owner.create(name: params[:owner][:name]).id
+      
+      @pet.update(params[:pet])
+    end
     redirect to "pets/#{@pet.id}"
   end
 end
